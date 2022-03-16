f51f978d99a631e925f5deeda6ba28e1e532f3cd
Gautam <61959145+gautam7-github@users.noreply.github.com>
2022-03-16 20:13:27 +0530

    changes to socket

--- Bot/SocketBot/socketBot.py
+++ Bot/SocketBot/socketBot.py
@@ -11,9 +11,11 @@ import pandas as pd
 import time
 import ta
 
-SOCKET = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m"
+pair = "btcusdt"
+time = "1m"
+SOCKET = f"wss://stream.binance.com:9443/ws/{pair}@kline_{time}"
 
-df = pd.DataFrame(columns=["close", "high", "low"])
+df = pd.DataFrame(columns=["open", "high", "low", "close"])
 alreadyIn = False
 
 
@@ -30,17 +32,8 @@ def onM(wso, msg):
     isClosed = candleData['x']
     if isClosed:
         print(f"O {open} H {high} L {low} C {close}")
-        df.loc[len(df)] = [float(close), float(high), float(low)]
+        df.loc[len(df)] = [float(open), float(high), float(low), float(close)]
         print(df)
-        if len(df) > 1:
-            rsi = ta.momentum.RSIIndicator.rsi(df["close"], window=2)
-            print(rsi)
-            # if rsi[-1] > 70 and alreadyIn:
-            #     print("SELL")
-            #     alreadyIn = False
-            # elif rsi[-1] < 30 and not alreadyIn:
-            #     print("BUY")
-            #     alreadyIn = True
 
 
 def onClose(wso):
@@ -48,12 +41,6 @@ def onClose(wso):
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("./data.csv", index_col=0)
-    print(df)
-    df["rsi"] = ta.momentum.RSIIndicator(
-        close=df["close"], window=2, fillna=False
-    ).rsi()
-    print(df)
-    # a = ws.WebSocketApp(
-    #     SOCKET, on_close=onClose, on_message=onM)
-    # a.run_forever()
+    a = ws.WebSocketApp(
+        SOCKET, on_close=onClose, on_message=onM)
+    a.run_forever()
